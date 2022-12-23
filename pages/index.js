f5c26d9c114e8ce515669e4efd439d3d39c136d2
javicastellon <entrepreneurshipaccelerator@gmail.com>
2022-12-23 16:56:28 -0500

    elementary.ai.v.1

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,88 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react'
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('')
+const [isGenerating, setIsGenerating] = useState(false)
+
+const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
+  
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+  console.log("OpenAI replied...", output.text)
+
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>elementary.ai</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>Plagiarism Free Essay Writer</h1>
+            <h1>elementary.ai</h1>
           </div>
           <div className="header-subtitle">
-            <h2>Enter the subject of your essay along with word count requirement and format (MLA, APA, Chicago)</h2>
+            <h2>elementary level explanations for even the most complex topics</h2>
           </div>
         </div>
-        <div className="prompt-container">
-        <textarea placeholder="start typing here" className="prompt-box" />
-        </div>
       </div>
+      <div className="prompt-container">
+          <textarea 
+          placeholder="start typing here...ex. explain molar mass, explain bruits, explain the war of 1812" 
+          className="prompt-box"
+          value={userInput} 
+          onChange={onUserChangedText}
+          />
+          <div className="prompt-buttons">
+    <a className="generate-button" onClick={callGenerateEndpoint}>
+      <div className="generate">
+        <p>Generate</p>
+      </div>
+    </a>
+  </div>
+  {apiOutput && (
+  <div className="output">
+    <div className="output-header-container">
+      <div className="output-header">
+        <h3>explanation</h3>
+      </div>
+    </div>
+    <div className="output-content">
+      <p>{apiOutput}</p>
+    </div>
+  </div>
+  )}
+        </div>
       <div className="badge-container grow">
         <a
           href="https://buildspace.so/builds/ai-writer"
           target="_blank"
           rel="noreferrer"
         >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
-          </div>
+        <div className="badge">
+        <Image src={buildspaceLogo} alt="buildspace logo" />
+        <p>build with buildspace</p>
+      </div>
         </a>
       </div>
     </div>
